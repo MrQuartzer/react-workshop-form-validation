40ac0415ff678d46f59705583c4be208a54f9b77
MrQuartzer <nantawat_yai@hotmail.com>
2022-05-19 13:21:35 +0700

    Add Color Status

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -12,25 +12,36 @@ const FormComponent = () =>{
     const [errorpassword,setErrorPassword] = useState('')
     const [errorconfirmPassword,setErrorConfirmPassword] = useState('รหัสผ่านไม่ตรงกัน')
 
+    const [userNameColor,setUserNameColor] = useState('') 
+    const [emailColor,setEmailColor] = useState('')
+    const [passwordColor,setPasswordColor] = useState('')
+    const [confirmPasswordColor,setConfirmPasswordColor] = useState('')
+
     const validateForm = (e)=>{
         e.preventDefault()
 
         if(userName.length>8){
             setErrorUserName('')
+            setUserNameColor('green')
         }else{
             setErrorUserName('ป้อนชื่อผู้ใช้จำนวนมากกว่า 8 ตัวอักษร')
+            setUserNameColor('red')
         }
 
         if(email.includes('@')){
             setEmail('')
+            setEmailColor('green')
         }else{
             setErrorEmail('รูปแบบอีเมลไม่ถูกต้อง')
+            setEmailColor('red')
         }
 
         if(password.length>8){
             setErrorPassword('')
+            setPasswordColor('green')
         }else{
             setErrorPassword('รหัสผ่านต้องมีจำนวน 8 ตัวอักษร')
+            setPasswordColor('red')
         }
     }
 
@@ -40,23 +51,23 @@ const FormComponent = () =>{
                 <h2>แบบฟอร์มลงทะเบียน</h2>
                 <div className="Form-Control">
                     <label>ชื่อผู้ใช้</label>
-                    <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)}></input>
-                    <small>{errorUserName}</small>
+                    <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)} style={{borderColor:userNameColor}}></input>
+                    <small style={{color:userNameColor}}>{errorUserName}</small>
                 </div>
                 <div className="Form-Control">
                     <label>อีเมล</label>
-                    <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
-                    <small>{erroremail}</small>
+                    <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} style={{borderColor:emailColor}}></input>
+                    <small style={{color:emailColor}}>{erroremail}</small>
                 </div>
                 <div className="Form-Control">
                     <label>รหัสผ่าน</label>
-                    <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
-                    <small>{errorpassword}</small>
+                    <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} style={{borderColor:passwordColor}}></input>
+                    <small style={{color:passwordColor}}>{errorpassword}</small>
                 </div>
                 <div className="Form-Control">
                     <label>ยืนยันรหัสผ่าน</label>
-                    <input type="text" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}></input>
-                    <small>{errorconfirmPassword}</small>
+                    <input type="password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} style={{borderColor:confirmPasswordColor}}></input>
+                    <small style={{color:confirmPasswordColor}}>{errorconfirmPassword}</small>
                 </div>
                 <button type="submit">ลงทะเบียน</button>
             </form>
